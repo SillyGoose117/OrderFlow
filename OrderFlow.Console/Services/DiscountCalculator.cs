8763a76c70b44feda7cecfef903bdfe0f56ffc72
Jakub Rycembel <wwx18194@student.warszawa.merito.pl>
2026-05-27 18:09:20 +0200

    Green: Correct discount limit implementation

--- OrderFlow.Console/Services/DiscountCalculator.cs
+++ OrderFlow.Console/Services/DiscountCalculator.cs
@@ -13,6 +13,8 @@ public class DiscountCalculator
             discount = order.TotalAmount > 5000 ? discount += 0.05m : discount;
         }
         discount = order.TotalAmount > 1000 ? discount += 0.05m : discount;
+        discount = order.TotalAmount > 10000 ? discount += 0.1m : discount; //Dobija do 25% rabatu (nawet więcej bo 30%)
+        discount = Math.Min(discount, 0.25m); //Ogranicza rabat do maksymalnnie 25% na potrzeby testu
         discount *= order.TotalAmount;
         return discount;
     }
