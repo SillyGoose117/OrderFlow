901a810e7fa55947b4f2ce7dc392613535638449
Jakub Rycembel <wwx18194@student.warszawa.merito.pl>
2026-05-27 14:59:50 +0200

    Red: Standard customer low value test

---
+++ OrderFlow.Tests/DiscountCalcTests.cs
@@ -0,0 +1,21 @@
+﻿using OrderFlow.Console.Models;
+
+namespace OrderFlow.Tests;
+
+public class DiscountCalcTests
+{
+    [Fact]
+    public void Calculate_StandardCustomer_NoDiscount()
+    {
+        //Arrange
+        var order = new Order {Customer = new Customer {IsVIP = false}};
+        var testProduct = new Product { Stock = 50};
+        var orderItemTest = new OrderItem { Product = testProduct, Quantity = 2 , UnitPrice = 50};
+        order.Items.Add(orderItemTest);
+        var calculator = new DiscountCalculator();
+        //Act
+        decimal discount = calculator.CalculateDiscount(order);
+        //Assert
+        Assert.Equal(0m, discount);
+    }
+}
