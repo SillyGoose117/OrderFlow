647051f5de01bf51a4cf03495f022969d21c6186
Jakub Rycembel <wwx18194@student.warszawa.merito.pl>
2026-05-27 15:15:36 +0200

    Red: VIP customer low value discount tests

--- OrderFlow.Tests/DiscountCalcTests.cs
+++ OrderFlow.Tests/DiscountCalcTests.cs
@@ -10,13 +10,23 @@ public class DiscountCalcTests
     {
         //Arrange
         var order = new Order {Customer = new Customer {IsVIP = false}};
-        var testProduct = new Product { Stock = 50};
-        var orderItemTest = new OrderItem { Product = testProduct, Quantity = 2 , UnitPrice = 50};
-        order.Items.Add(orderItemTest);
         var calculator = new DiscountCalculator();
         //Act
         decimal discount = calculator.CalculateDiscount(order);
         //Assert
         Assert.Equal(0m, discount);
+    }
+
+    [Fact]
+    public void Calculate_VIPCustomer_Discount()
+    {
+        //Arrange
+        var order = new Order {Customer = new Customer {IsVIP = true}};
+        var orderItemTest = new OrderItem { Quantity = 2 , UnitPrice = 50 };
+        var calculator = new DiscountCalculator();
+        //Act
+        decimal discount = calculator.CalculateDiscount(order);
+        //Assert
+        Assert.Equal(10m, discount);
     }
 }
