6038fec9d1c6c1a8017f73938d587b1e673cf5c7
Jakub Rycembel <wwx18194@student.warszawa.merito.pl>
2026-05-27 16:28:28 +0200

    Red: Caluclate high value order discount test

--- OrderFlow.Tests/DiscountCalcTests.cs
+++ OrderFlow.Tests/DiscountCalcTests.cs
@@ -32,4 +32,18 @@ public class DiscountCalcTests
         //Assert
         Assert.Equal(10m, discount);
     }
+
+    [Fact]
+    public void Calculate_HighValue_Discount()
+    {
+        //Arrange
+        var order = new Order {Customer = new Customer {IsVIP = false}};
+        var orderItemTest = new OrderItem { Quantity = 2 , UnitPrice = 1000 };
+        order.Items.Add(orderItemTest);
+        var calculator = new DiscountCalculator();
+        //Act
+        decimal discount = calculator.CalculateDiscount(order);
+        //Assert
+        Assert.Equal(100m, discount);
+    }
 }
