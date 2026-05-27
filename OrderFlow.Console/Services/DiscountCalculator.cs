39aa6c70e536ffa7c9615dcc67d9800162f2857a
Jakub Rycembel <wwx18194@student.warszawa.merito.pl>
2026-05-27 15:06:33 +0200

    Green: Standard customer low value discount implementation

---
+++ OrderFlow.Console/Services/DiscountCalculator.cs
@@ -0,0 +1,8 @@
+﻿using OrderFlow.Console.Models;
+
+namespace OrderFlow.Console.Services;
+
+public class DiscountCalculator
+{
+    public decimal CalculateDiscount(Order order) => 0m;
+}
--- OrderFlow.Tests/DiscountCalcTests.cs
+++ OrderFlow.Tests/DiscountCalcTests.cs
@@ -1,4 +1,5 @@
 ﻿using OrderFlow.Console.Models;
+using OrderFlow.Console.Services;
 
 namespace OrderFlow.Tests;
 
