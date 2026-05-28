ef216cc9c57044a4637ed761f029eaacef94d2f4
Jakub Rycembel <wwx18194@student.warszawa.merito.pl>
2026-05-28 23:11:51 +0200

    Lab 5 - Zadanie 3.4 (Testy mockujące HttpsMessageHandler): Implementacja CurrencyService. Utworzono klasę TestHttpMessageHandler. Utworzone testy jednostkowe dla HttpsMessageHandler. Pozsotała integracja z OrderFlow oraz pokaz użycia w program.cs

---
+++ OrderFlow.Console/Services/CurrencyService.cs
@@ -0,0 +1,103 @@
+﻿using System.Net;
+using System.Text.Json.Serialization;
+using System.Net.Http.Json;
+
+namespace OrderFlow.Console.Services;
+
+public interface ICurrencyService
+{
+    Task<decimal?> GetRateAsync(string currencyCode);
+    Task<decimal> ConvertAsync(decimal amount, string fromCurrency, string toCurrency);
+}
+
+public class CurrencyService : ICurrencyService
+{
+    private readonly HttpClient _httpClient;
+
+    public CurrencyService(HttpClient client)
+    {
+        _httpClient = client;
+    }
+
+    public async Task<decimal?> GetRateAsync(string currencyCode)
+    {
+        if (currencyCode.ToUpper() == "PLN")
+        {
+            return 1.0m;
+        }
+
+        var url = $"https://api.nbp.pl/api/exchangerates/rates/A/{currencyCode}/?format=json";
+
+        try
+        {
+            var response = await _httpClient.GetFromJsonAsync<NbpRateResponse>(url);
+            return response?.Rates.FirstOrDefault()?.Mid;
+        }
+        catch (HttpRequestException ex) when (ex.StatusCode == HttpStatusCode.NotFound)
+        {
+            return null;
+        }
+        catch (HttpRequestException ex) 
+        {
+            throw new CurrencyServiceException($"Błąd pobierania kursu: {ex.Message}", ex);
+        }
+        catch (TaskCanceledException)
+        {
+            throw new CurrencyServiceException("Timeout przy pobieraniu kursu");
+        }
+    }
+    
+    public async Task<decimal> ConvertAsync(decimal amount, string fromCurrency, string toCurrency)
+    {
+        var fromCurrencyNumber = await GetRateAsync(fromCurrency);
+        if (fromCurrencyNumber == null)
+        {
+            throw new CurrencyServiceException("Błąd pobierania kursu.");
+        }
+        var toCurrencyNumber = await GetRateAsync(toCurrency);
+        if (toCurrencyNumber == null)
+        {
+            throw new CurrencyServiceException("Błąd pobierania kursu.");
+        }
+        var convertedAmount = (amount * fromCurrencyNumber.Value) / toCurrencyNumber.Value;
+        return convertedAmount;
+    }
+}
+
+[Serializable]
+public class CurrencyServiceException : Exception
+{
+    public CurrencyServiceException(string message) : base(message)
+    {}
+
+    public CurrencyServiceException (string message, Exception innerException)
+        : base (message, innerException)
+    {}  
+}
+
+public class NbpRateResponse
+{
+    [JsonPropertyName("table")]
+    public string Table { get; set; } = "";
+
+    [JsonPropertyName("currency")]
+    public string Currency { get; set; } = "";
+
+    [JsonPropertyName("code")]
+    public string Code { get; set; } = "";
+
+    [JsonPropertyName("rates")]
+    public List<NbpRate> Rates { get; set; } = new();
+}
+
+public class NbpRate
+{
+    [JsonPropertyName("no")]
+    public string No { get; set; } = "";
+
+    [JsonPropertyName("effectiveDate")]
+    public DateOnly EffectiveDate { get; set; }
+
+    [JsonPropertyName("mid")]
+    public decimal Mid { get; set; }
+}
---
+++ OrderFlow.Tests/TestHttpMessageHandler.cs
@@ -0,0 +1,153 @@
+﻿using System.Net;
+using System.Net.Http.Json;
+using Microsoft.Extensions.Http.Logging;
+using Moq;
+using OrderFlow.Console.Services;
+
+namespace OrderFlow.Tests;
+
+public class TestHttpMessageHandler(Func<HttpResponseMessage> responseFactory) : HttpMessageHandler
+{
+    public int CallCount { get; private set; } = 0; // Do sprwadzenia specjalnegu przypadku zPLN
+    public Uri? CorrectUri { get; private set; }
+    
+    protected override Task<HttpResponseMessage> SendAsync(HttpRequestMessage request,
+        CancellationToken cancellationToken)
+    {
+        CallCount++;
+        CorrectUri = request.RequestUri;
+        return Task.FromResult(responseFactory());
+    }
+}
+
+public class ConversionRatesTest
+{
+    [Fact]
+    public async Task Check_CorrectPath_ReturnsConversion() // (amount * fromCurrencyNumber.Value) / toCurrencyNumber.Value
+    {
+        //Arrange
+        var amount = 5m;
+        var fromCurrency = "EUR";
+        var toCurrency = "USD";
+        var fakeNbpResponse = new NbpRateResponse()
+        {
+            Rates = new List<NbpRate>
+            {
+                new NbpRate{Mid = 4.50m}
+            }
+        };
+        var handler = new TestHttpMessageHandler(() => new HttpResponseMessage
+        {
+            StatusCode = HttpStatusCode.OK,
+            Content = JsonContent.Create(fakeNbpResponse)
+        });
+        var client = new HttpClient(handler);
+        var currencyServices = new CurrencyService(client);
+        //Act
+        var rates = await currencyServices.ConvertAsync(amount, fromCurrency, toCurrency);
+        //Assert
+        Assert.Equal(5m, rates);
+    }
+
+    [Fact]
+    public async Task Check_SpecialCase_NoApi()
+    {
+        //Arrange
+        var baseCurrency = "PLN";
+        var handler = new TestHttpMessageHandler(() => new HttpResponseMessage());
+        var client = new HttpClient(handler);
+        var currencyServices = new CurrencyService(client);
+        //Act
+        var rate = await currencyServices.GetRateAsync(baseCurrency);
+        //Assert
+        Assert.Equal(1, rate);
+        Assert.Equal(0, handler.CallCount);
+    }
+
+    [Fact]
+    public async Task Check_ErrorHandling_404()
+    {
+        var fakeCurrency = "GLORB";
+        var handler = new TestHttpMessageHandler(() => new HttpResponseMessage
+        {
+            StatusCode = HttpStatusCode.NotFound,
+        });
+        var client = new HttpClient(handler);
+        var currencyServices = new CurrencyService(client);
+        //Act
+        var checkForHandler = await currencyServices.GetRateAsync(fakeCurrency);
+        //Assert
+        Assert.Null(checkForHandler);
+    }
+
+    [Fact]
+    public async Task Check_ErrorHandling_500()
+    {
+        var fakeCurrency = "GLORB";
+        var handler = new TestHttpMessageHandler(() => new HttpResponseMessage
+        {
+            StatusCode = HttpStatusCode.InternalServerError,
+        });
+        var client = new HttpClient(handler);
+        var currencyServices = new CurrencyService(client);
+        //Act
+        var exception = await Assert.ThrowsAsync<CurrencyServiceException>(() => currencyServices.GetRateAsync(fakeCurrency));
+        //Assert
+        Assert.Contains("Błąd pobierania kursu:",  exception.Message);
+    }
+
+    [Fact]
+    public async Task Check_CorrectRates_ValuesNotNull()
+    {
+        //Arrange
+        var fromCurrency = "EUR";
+        var toCurrency = "USD";
+        var fakeNbpResponse = new NbpRateResponse()
+        {
+            Rates = new List<NbpRate>
+            {
+                new NbpRate{Mid = 4.50m}
+            }
+        };
+        var handler = new TestHttpMessageHandler(() => new HttpResponseMessage
+        {
+            StatusCode = HttpStatusCode.OK,
+            Content = JsonContent.Create(fakeNbpResponse)
+        });
+        var client = new HttpClient(handler);
+        var currencyServices = new CurrencyService(client);
+        //Act
+        var a = await currencyServices.GetRateAsync(fromCurrency);
+        var b = await currencyServices.GetRateAsync(toCurrency);
+        //Assert
+        Assert.Equal(4.5m, a);
+        Assert.Equal(4.50m, b);
+    }
+    
+    [Fact]
+    public async Task Check_Path_CorrectUrl()
+    {
+        //Arrange
+        var fromCurrency = "EUR";
+        var expectedUrl = $"https://api.nbp.pl/api/exchangerates/rates/A/{fromCurrency}/?format=json";
+        var fakeNbpResponse = new NbpRateResponse()
+        {
+            Rates = new List<NbpRate>
+            {
+                new NbpRate { Mid = 4.50m }
+            }
+        };
+        var handler = new TestHttpMessageHandler(() => new HttpResponseMessage
+        {
+            StatusCode = HttpStatusCode.OK,
+            Content = JsonContent.Create(fakeNbpResponse)
+        });
+        var client = new HttpClient(handler);
+        var currencyServices = new CurrencyService(client);
+        //Act
+        await currencyServices.GetRateAsync(fromCurrency);
+        //Assert
+        Assert.NotNull(handler.CorrectUri);
+        Assert.Contains(expectedUrl, handler.CorrectUri.ToString());
+    }
+}
